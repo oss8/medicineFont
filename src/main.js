1f59be3a3dac098fce56c53dd0ab22c3df0df468
朱衢林 <0xjoker@bombvote.com>
2017-04-21 14:12:07 +0800

    mint-ui add

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import 'element-ui/lib/theme-default/index.css'
 import 'nprogress/nprogress.css'
 import VueRouter from 'vue-router';
 import App from './App'
 import router from './router'
 // import {Table,Form,TableColumn,Popover,Tooltip,Input,Button,FormItem} from 'element-ui'
-import ElementUI from 'element-ui'
+import Mint from 'mint-ui';
 import axios from 'axios';
 // import VueAxios from 'vue-axios';
 import './style/common.scss'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Mint)
 Vue.use(VueRouter);
 // Vue.use(VueAxios, axios)
 // Vue.use(Table)
