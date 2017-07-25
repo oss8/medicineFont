87fa1bad0f7a0feed0560d69ed91449939d6e265
朱衢林 <0xjoker@bombvote.com>
2017-07-25 14:53:00 +0800

    fix

--- src/components/bind.vue
+++ src/components/bind.vue
@@ -32,7 +32,7 @@
                 let token = this.$route.query.token;
                 if(!token) {util.toast('token过期，请重新进入我的健康');return;}
                 let params = {PublicUserInputDetailCode:{token:token,cardNo:this.fromData.carNo,medicalNo:this.fromData.medicalNo}};
-                if(this.fromData.cardNo.length > 0 || this.fromData.medicalNo.length > 0) {
+                if(this.fromData.carNo.length > 0 || this.fromData.medicalNo.length > 0) {
                     util.postData('baseServices/PublicUserInputDetailCode',params)
                     .then((data)=> {
                         this.$router.replace({
